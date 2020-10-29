dc78b1a601ae5ccb51c681563d304fe85768005a
Kenli8504 <kli22@stuy.edu>
2020-10-28 20:26:45 -0400

    Just wrote what I think could be good, no tests yet, will test later

---
+++ BankAccount.java
@@ -0,0 +1,47 @@
+public class BankAccount{
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(int a, String b){
+    balance = 0;
+    accountID = a;
+    password = b;
+  }
+
+  public int ReturnID () {
+    return accountID;
+  }
+
+  public String ReturnPassWord () {
+    return password;
+  }
+
+  public boolean deposit(double amount){
+    if (amount >= 0){
+      balance = balance + amount;
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  public boolean withdraw(double amount){
+    if (amount < 0 || amount > balance){
+      return false;
+    }
+    else{
+      balance = balance - amount;
+      return true;
+    }
+  }
+
+  public String toString(){
+    String a = "";
+    a = a + accountID;
+    a = a + " \t ";
+    a = a + balance;
+    return a;
+  }
+}
